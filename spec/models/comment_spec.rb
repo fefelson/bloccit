9a637c55180da8f96d1f2c4a8c9254d583e45323
Xander Miller <xander.miller@gmail.com>
2016-03-30 21:02:36 +0000

    tests associations using shoulda gem

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe Comment, type: :model do
   describe "attributes" do # Using Shoulda matchers http://matchers.shoulda.io/docs/v3.1.1/
     it { should have_db_column(:body).of_type(:text) }
   end
+  
+  describe 'associations' do # Using Shoulda matchers http://matchers.shoulda.io/docs/v3.1.1/
+    it { should belong_to(:post) }
+  end
 
 end
