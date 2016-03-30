f7c312fdb4dee8e9208d24a8bf47eb9f6144b49c
Xander Miller <xander.miller@gmail.com>
2016-03-30 20:51:39 +0000

    installs shoulda gem and cleans up rails_helper.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Comment, type: :model do
   let(:post) {Post.create!(title: "New Post Title", body: "New Post Body")}
   let(:comment) { Comment.create!(body: 'Comment Body', post: post)}
 
-  describe "attributes" do
-    it "responds to body" do
-      expect(comment).to respond_to(:body)
+  describe "attributes" do # Using Shoulda matchers http://matchers.shoulda.io/docs/v3.1.1/
+    it do
+      should have_db_column(:body).of_type(:text)
     end
   end
 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-
   let(:post) {Post.create!(title: "New Post Title", body: "New Post Body")}
 
-  describe "attributes" do
+  describe "attributes" do # Using Shoulda matchers http://matchers.shoulda.io/docs/v3.1.1/
     it "responds to title" do
       expect(post).to respond_to(:title)
     end
@@ -12,6 +11,5 @@ RSpec.describe Post, type: :model do
     it "responds to body" do
       expect(post).to respond_to(:body)
     end
-
   end
 end
