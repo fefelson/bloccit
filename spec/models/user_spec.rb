0320a44b158330cc2a1bd5d85553fac40d8b5881
Edward Walsh <edwrdwalsh@gmail.com>
2016-04-05 18:03:58 -0400

    Checkpoint 36

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,10 +9,12 @@ RSpec.describe User, type: :model do
 
   #Shoulda tests for email
   it { is_expected.to validate_presence_of(:email)}
-  it { is_expected.to validate_uniqueness_of(:email)}
+  # it { is_expected.to validate_uniqueness_of(:email)}
   it { is_expected.to validate_length_of(:email).is_at_least(3)}
   it { is_expected.to allow_value("user@bloccit.com").for(:email)}
-
+  it "validates uniqueness" do
+    expect(user).to validate_uniqueness_of(:email)
+  end
   #Shoulda tests for password
   it { is_expected.to validate_presence_of(:password)}
   it { is_expected.to have_secure_password }
