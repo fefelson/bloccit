28d0c0f2a4bbd50f78fad603b6b6723605c47407
Edward Walsh <edwrdwalsh@gmail.com>
2016-04-05 10:50:57 -0400

    35 Assignment topic validation

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
   it {is_expected.to have_many(:posts)}
-  
+
+  it { is_expected.to validate_length_of(:name).is_at_least(5)}
+  it { is_expected.to validate_length_of(:description).is_at_least(15)}
+
   describe "attributes" do
     it { should have_db_column(:name).of_type(:string)}
     it { should have_db_column(:description).of_type(:text)}
