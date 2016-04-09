797969d86cf88ce692f21331744e367779793561
Xander Miller <xander.miller@gmail.com>
2016-04-09 22:11:47 +0000

    adds dependent destroy to spec

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Topic, type: :model do
   end
   
   describe 'associations' do
-    it {is_expected.to have_many(:posts)}
+    it { is_expected.to have_many(:posts).dependent(:destroy) }
   end
 
 end
