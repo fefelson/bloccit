19a68a8c0d10f372f38d230af0f1f26e15eafbab
Xander Miller <xander.miller@gmail.com>
2016-04-09 22:03:39 +0000

    topic uses shoulda associations

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,17 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
   
   describe "attributes" do
     it { should have_db_column(:name).of_type(:string)}
     it { should have_db_column(:description).of_type(:text)}
-    it { should have_db_column(:public).of_type(:boolean)}
-
-    it "is public by default" do
-      expect(topic.public).to be(true)
-
-    end
+    it { should have_db_column(:public).of_type(:boolean).with_options({default: true}) }
   end
   
   describe 'associations' do
