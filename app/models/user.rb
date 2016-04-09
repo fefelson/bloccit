69ae16874890e4fad5a173d442e62f579307f6ef
Xander Miller <xander.miller@gmail.com>
2016-04-09 23:52:04 +0000

    cleans up user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  before_save { self.email = email.downcase }
+  before_save -> (){ self.email = email.downcase }, if: ->(){ email.present? }
 
   validates :name, length: {minimum: 1, maximum: 100}, presence: true
 
