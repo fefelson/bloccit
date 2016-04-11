b5fa66943ffe3b759f8e5b6fb1f1613dec50c0d1
Xander Miller <xander.miller@gmail.com>
2016-04-11 17:25:57 +0000

    adds missing specs to users spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe User, type: :model do
     it { should have_db_column(:name).of_type(:string) }
     it { should have_db_column(:email).of_type(:string) }
   end
+  
+  describe 'associations' do
+    it { is_expected.to have_many(:posts) }
+  end
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:password)}
@@ -15,6 +19,7 @@ RSpec.describe User, type: :model do
     it { is_expected.to allow_value("user@bloccit.com").for(:email)}
     it { is_expected.to validate_presence_of(:email)}
     it { is_expected.to validate_presence_of(:name)}
+    it { is_expected.to validate_presence_of(:user) }
     it { is_expected.to validate_length_of(:name).is_at_least(1)}
     it { should validate_uniqueness_of(:email).case_insensitive }
   end
