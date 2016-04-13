1692f77a6eefcaceb82678127cfd57ae20bfe8dc
Xander Miller <xander.miller@gmail.com>
2016-04-13 22:19:10 +0000

    adds presence validation for role

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:name)}
     it { is_expected.to validate_length_of(:name).is_at_least(1)}
     #role
-    
+    it { is_expected.to validate_presence_of(:role)}
   end
 
 end
