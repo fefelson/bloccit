70545da1a2bddc91bb17af80f1bc987104023339
Xander Miller <xander.miller@gmail.com>
2016-04-13 21:39:09 +0000

    you don't have to test invalid users when using shoulda matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -68,19 +68,6 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe "invalid user" do
-     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-     let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
-
-     it "should be an invalid user due to blank name" do
-       expect(user_with_invalid_name).to_not be_valid
-     end
-
-     it "should be an invalid user due to blank email" do
-       expect(user_with_invalid_email).to_not be_valid
-     end
-   end
-
   describe 'validations' do
     it { is_expected.to validate_presence_of(:password)}
     it { is_expected.to have_secure_password }
