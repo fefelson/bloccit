ce886a81265b15118c9e5cd2321c5c05f0ef67fd
Xander Miller <xander.miller@gmail.com>
2016-04-13 21:37:02 +0000

    users Rails 4 style database restrictions to provide default values

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,17 @@
 class User < ActiveRecord::Base
-
+  #enum helper has to be before validation for validation to work
+  enum role: [:member, :admin]
   has_many :posts
 
   before_save -> { self.email = email.downcase }, if: -> { email.present? }
-  before_save { self.role ||= :member }
 
   validates :name, length: {minimum: 1, maximum: 100}, presence: true
 
   validates :password, presence: true, length: {minimum: 6}, if: "password_digest.nil?"
   validates :password, length: {minimum: 6}, allow_blank: true
+  
+  validates :role, presence: true,
+    inclusion: { in: roles.keys , message: "%{value} is not a valid role" }
 
   validates :email,
     presence: true,
@@ -17,6 +20,5 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  enum role: [:member, :admin]
 
 end
