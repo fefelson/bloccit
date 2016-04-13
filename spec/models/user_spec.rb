45114f690f1ea0a94174a5dcb169609701b5f6e4
Xander Miller <xander.miller@gmail.com>
2016-04-13 21:53:17 +0000

    adds associations spec block to user removes messy specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
-  let(:user) {User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password")}
-
-  it { is_expected.to have_many(:posts)}
-
-  it { is_expected.to validate_presence_of(:name)}
-  it { is_expected.to validate_length_of(:name).is_at_least(1)}
-
-  #Shoulda tests for email
-  it { is_expected.to validate_presence_of(:email)}
-  # it { is_expected.to validate_uniqueness_of(:email)}
-  it { is_expected.to validate_length_of(:email).is_at_least(3)}
-  it { is_expected.to allow_value("user@bloccit.com").for(:email)}
-  it "validates uniqueness" do
-    expect(user).to validate_uniqueness_of(:email).case_insensitive
-  end
   
   describe "attributes" do
     it { should have_db_column(:name).of_type(:string) }
@@ -25,18 +9,25 @@ RSpec.describe User, type: :model do
     it { should define_enum_for(:role) }
   end
   
+  describe 'associations' do
+    it { should have_many(:posts) }
+  end
+  
   describe 'validations' do
+    #password
     it { is_expected.to validate_presence_of(:password)}
     it { is_expected.to have_secure_password }
     it { is_expected.to validate_length_of(:password).is_at_least(6)}
-    
+    #email
     it { is_expected.to validate_length_of(:email).is_at_least(3)}
     it { is_expected.to allow_value("user@bloccit.com").for(:email)}
     it { is_expected.to validate_presence_of(:email)}
     it { should validate_uniqueness_of(:email).case_insensitive }
-    
+    #name
     it { is_expected.to validate_presence_of(:name)}
     it { is_expected.to validate_length_of(:name).is_at_least(1)}
+    #role
+    
   end
 
 end
