6b7437a4a170cf1aa3fc091f856382e1024d312a
Xander Miller <xander.miller@gmail.com>
2016-04-14 18:15:29 +0000

    completely refactors authorization system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  rescue_from Exceptions::NotAuthorizedError, with: :user_not_authorized
 
   private
 
@@ -10,5 +12,11 @@ class ApplicationController < ActionController::Base
       redirect_to new_session_path
     end
   end
+  
+  def user_not_authorized exception
+    flash[:alert] = "You are not authorized to do that."
+    redirect_to(request.referrer || root_path)
+  end
+  
   
 end
